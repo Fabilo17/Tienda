73ad32e0096579516ab2c303844dc229fd025e76
Fabian <Fabian@DESKTOP-9HSA45N>
2022-10-07 16:29:19 -0600

    Modificacion de semana #4

---
+++ src/main/java/com/tienda/controller/PersonaController.java
@@ -0,0 +1,29 @@
+package com.tienda.controller;
+import com.tienda.entity.Pais;
+import com.tienda.entity.Persona;
+import com.tienda.service.IPaisService;
+import com.tienda.service.IPersonaService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class PersonaController {
+    @Autowired
+    private IPersonaService personaService;
+    @Autowired
+    private IPaisService paisService;
+    
+    @GetMapping("/persona")
+    public String index (Model model){
+        List<Persona> listaPersona = personaService.getAllPersona();
+        model.addAttribute("titulo", "Tabla Personas");
+        model.addAttribute("personas", listaPersona);
+        return "personas";
+    }
+}
---
+++ src/main/java/com/tienda/entity/Persona.java
@@ -0,0 +1,76 @@
+package com.tienda.entity;
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "presona")
+public class Persona implements Serializable{
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private long id;
+    
+    private String nombre;
+    private String apellidos1;
+    private String apellidos2;
+    private String telefono;
+    private String email;
+    
+    @ManyToOne
+    @JoinColumn(name="paises_id")
+    private Pais pais;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos1() {
+        return apellidos1;
+    }
+
+    public void setApellidos1(String apellidos1) {
+        this.apellidos1 = apellidos1;
+    }
+
+    public String getApellidos2() {
+        return apellidos2;
+    }
+
+    public void setApellidos2(String apellidos2) {
+        this.apellidos2 = apellidos2;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
